a899c77deb16d93df6f7afcfd7103a91e249df67
JEverett <jonathan@jonathaneverettcreative.com>
2019-06-02 21:08:46 -0500

    Created react app within top-level directory, in folder 'clicky.' Created initial layout of folders and started creating components, beginning with a simple navbar. Tested render in App with success. Moving on to Jumbotron component next, then a wrapper/container for images.

---
+++ clicky/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import NavBar from "./components/NavBar";
+
+
+
+class App extends Component {
+  // state = {
+  //   characters
+  // };
+  render() {
+    return (
+      <NavBar></NavBar>
+    )
+  }
+}
+
+
+
+export default App;
---
+++ clicky/src/components/NavBar/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function NavBar() {
+    return (
+        <nav className="navbar navbar-dark bg-dark">
+            <span className="navbar-brand mb-0 h1">
+                Game of Clicks
+            </span>
+        </nav>
+    )
+}
+
+export default NavBar;

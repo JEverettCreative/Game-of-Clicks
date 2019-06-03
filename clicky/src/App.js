748ff83fc3e1ec77514d8dac34545a32c24bd74c
JEverett <jonathan@jonathaneverettcreative.com>
2019-06-03 10:36:49 -0500

    Moved Imgs folder to public file, and set the json file to reference that path. Isolated the background image from that folder and moved it into the folder with it's Jumbotron component, since it cannot pull from public outside of source. Set App.js to render the images, but no styling has been added yet.

--- clicky/src/App.js
+++ clicky/src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
 import NavBar from "./components/NavBar";
 import Jumbotron from './components/Jumbotron';
+import Headshot from "./components/Headshot";
+import characters from "./characters.json";
 
 
 
 class App extends Component {
-  // state = {
-  //   characters
-  // };
+  state = {
+    characters
+  };
+
+
   render() {
     return (
       <>
       <NavBar></NavBar>
       <Jumbotron></Jumbotron>
+      {this.state.characters.map(characters => (
+        <Headshot
+          id={characters.id}
+          key={characters.id}
+          name={characters.name}
+          image={characters.image}
+        />
+      ))}
       </>
     )
   }

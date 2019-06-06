e68508d8b088754caa8f06fab4a40e124517c9a8
JEverett <jonathan@jonathaneverettcreative.com>
2019-06-06 12:16:37 -0500

    Updates

---
+++ precache-manifest.95f0afe56ad88d820da59bce570ea2b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91eaa4009f3b57e6ff79d34654e5c9ed",
+    "url": "/Game-of-Clicks/index.html"
+  },
+  {
+    "revision": "7522a801b574bcfb9607",
+    "url": "/Game-of-Clicks/static/css/main.2f8542c3.chunk.css"
+  },
+  {
+    "revision": "4743c039c04edea7e174",
+    "url": "/Game-of-Clicks/static/js/2.1db7aab6.chunk.js"
+  },
+  {
+    "revision": "7522a801b574bcfb9607",
+    "url": "/Game-of-Clicks/static/js/main.171c4846.chunk.js"
+  },
+  {
+    "revision": "3313b5ba536593f64d4b",
+    "url": "/Game-of-Clicks/static/js/runtime~main.6b66b836.js"
+  },
+  {
+    "revision": "6dc17cf2b5d9c8129cadab4561716bb7",
+    "url": "/Game-of-Clicks/static/media/starkbanner.6dc17cf2.jpg"
+  }
+]);
